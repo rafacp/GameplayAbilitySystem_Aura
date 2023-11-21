16ae7dd22781a141a6ffd67c6fbc5f04cce8037e
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2023-11-21 20:29:00 -0300

    Highlight Interface creation

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -4,14 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+
+	// Inherited via IEnemyInterface
+public:
+	virtual void HightlightActor() override;
+
+	virtual void UnHighlightActor() override;
+
 };
