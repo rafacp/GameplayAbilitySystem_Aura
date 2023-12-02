22a6f423903c137a66217788ddb994a6e135e3f7
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2023-12-02 11:46:40 -0300

    65.Initialize attributes from a data table

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -25,7 +25,7 @@ public:
 	UAttributeSet* GetAttributeSet() { return AttributeSet; }
 
 protected:
-	UPROPERTY()
+	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 
 	UPROPERTY()
