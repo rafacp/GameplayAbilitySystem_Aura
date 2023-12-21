1bc556853c7cbb9ab5b0d71fdbdffbd0b4aa2d12
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2023-12-21 11:45:08 -0300

    130. Curve Tables - CSV and JSON

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright RCP
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
