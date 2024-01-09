76824f8e10a4a59cc91bbe1d41afdfc0dcff241f
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2024-01-09 08:30:04 -0300

    245. Cooldown Async Task

---
+++ Source/Aura/Public/AbilitySystem/AsyncTasks/WaitCooldownChange.h
@@ -0,0 +1,50 @@
+// Copyright RCP
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GameplayTagContainer.h"
+#include "WaitCooldownChange.generated.h"
+
+
+class UAbilitySystemComponent;
+struct FGameplayEffectSpec;
+struct FActiveGameplayEffectHandle;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCooldownChangeSignature, float, TimeRemaining);
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UWaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownStart;
+	
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownEnd;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UWaitCooldownChange* WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& InCooldownTag);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+
+protected:
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> ASC;
+	
+	UPROPERTY()
+	FGameplayTag CooldownTag;
+
+	void CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount);
+
+	void OnActiveEffectAdded(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle);
+};
