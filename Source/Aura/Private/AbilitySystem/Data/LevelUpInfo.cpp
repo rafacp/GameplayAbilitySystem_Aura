cda2d2e81e79f32b4ea2c0eae4e1e76cb0ee1c8d
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2024-01-09 19:53:55 -0300

    250. Level Up Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,35 @@
+// Copyright RCP
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	/*for (size_t i = 0; i < LevelUpInformation.Num(); i++)
+	{
+		if (XP <= LevelUpInformation[i].LevelUpRequirement)
+		{
+			return i+1;
+		}
+	}
+	return 1;*/
+
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		//LevelUpInformation[0] is meaningless
+		//LevelUpInformation[1] is first level and so on
+		if (LevelUpInformation.Num() - 1 <= Level) return Level; //got to cap
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			Level++;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
