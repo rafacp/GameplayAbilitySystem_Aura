80b3d8cf6fb74fdf1874a660b13a7830a5485355
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2024-01-19 22:15:15 -0300

    307. Determining Debuff

--- Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
+++ Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -18,4 +18,6 @@ public:
 	UExecCalc_Damage();
 
 	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+
+	void DetermineDebuff(const FGameplayEffectCustomExecutionParameters& ExecutionParams, const FGameplayEffectSpec& Spec, FAggregatorEvaluateParameters EvaluationParameters, const TMap<FGameplayTag, FGameplayEffectAttributeCaptureDefinition>& InTagsToDefs) const;
 };
