1f748e589333674e535dbfd44435fc7669bd68d7
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2024-01-25 07:53:14 -0300

    329. Additional Targets

--- Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -5,6 +5,7 @@
 #include "GameFramework/Character.h"
 #include <Kismet/KismetSystemLibrary.h>
 #include <Interaction/CombatInterface.h>
+#include <AbilitySystem/AuraAbilitySystemLibrary.h>
 
 
 void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
@@ -59,4 +60,27 @@ void UAuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
 			}
 		}
 	}
+}
+
+void UAuraBeamSpell::StoreAdditionalTargets(TArray<AActor*>& OutAdditionalTargets)
+{
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(GetAvatarActorFromActorInfo());
+	ActorsToIgnore.Add(MouseHitActor);
+
+	TArray<AActor*> OverlappingActors;
+	UAuraAbilitySystemLibrary::GetLivePlayersWithinRadius(
+		GetAvatarActorFromActorInfo(),
+		OverlappingActors,
+		ActorsToIgnore,
+		850.f,
+		MouseHitActor->GetActorLocation());
+
+	int32 NumAdditionalTargets = 5;// FMath::Min(MaxNumShockTargets, GetAbilityLevel() - 1);
+
+	UAuraAbilitySystemLibrary::GetClosestTargets(
+		NumAdditionalTargets, 
+		OverlappingActors, 
+		OutAdditionalTargets, 
+		MouseHitActor->GetActorLocation());
 }
--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -98,6 +98,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayMechanics", meta = (DefaultToSelf = "WorldContextObject"))
 	static void GetLivePlayersWithinRadius(const UObject* WorldContextObject, TArray<AActor*>& OutOverlappingActors, const TArray<AActor*>& ActorsToIgnore, float Radius, const FVector& SphereOrigin);
 
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayMechanics")
+	static void GetClosestTargets(int32 MaxTargets, const TArray<AActor*>& Actors, TArray<AActor*>& OutClosestTargets, const FVector& Origin);
+
 	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|GameplayMechanics")
 	static bool IsNotFriend(AActor* FirstActor, AActor* SecondActor);
 	
